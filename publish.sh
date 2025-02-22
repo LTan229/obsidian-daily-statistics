97086933a5186c63e40b0302b30e94cfbf0d0a2a
yefengr <yefengr@qq.com>
2025-02-22 09:29:33 +0800

    更新发布脚本

--- publish.sh
+++ publish.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# 定义一个执行命令的函数，它会先打印命令然后再执行
+execute_cmd() {
+    echo "Executing: $@"
+    echo "----------------------------------------"
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        echo "Error: Command failed with status $status"
+        exit $status
+    fi
+    echo "----------------------------------------"
+}
+
 # 从 manifest.json 读取版本号
 VERSION=$(cat manifest.json | grep \"version\" | cut -d'"' -f4)
 
@@ -10,9 +23,9 @@ echo "git push origin $VERSION"
 echo "----------------------------------------"
 
 # 创建带注释的 git tag
-git tag -a $VERSION -m "$VERSION"
+execute_cmd git tag -a "$VERSION" -m "$VERSION"
 
 # 推送 tag 到远程仓库
-git push origin $VERSION
+execute_cmd git push origin "$VERSION"
 
 echo "Successfully created and pushed tag $VERSION"
